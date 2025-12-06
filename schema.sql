09d28c5847615b95d24a5b19dd04ed2635313f2e
Mitushi-6 <mitushirmanoj123@gmail.com>
2025-12-06 14:18:04 +0530

    Create database and update schema for movies_data

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
+create database movie_pipeline;
 use movie_pipeline;
 CREATE TABLE IF NOT EXISTS movies_data (movieId INT PRIMARY KEY, title VARCHAR(255),genres VARCHAR(255),director VARCHAR(255), release_date date , rating FLOAT );
-select*from movies_data limit 5;
+
 
